272e3a0a40e345354052942c34992dfa5f3e7a39
Guilherme <guilhermedejesusperdigao@gmail.com>
2026-04-29 13:11:31 +0000

    Utilizei a estrutura de repetição Do-while para simular a movimentação da peça Rainha do Xadrez.

--- xadrez.c
+++ xadrez.c
@@ -22,7 +22,10 @@ int main() {
     
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
+    do {
+        printf("Esquerda.\n");
+    } while (rainha <= 8);
+    
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
