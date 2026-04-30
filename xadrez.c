261ec8f3544f8e614a411c45a9689c07c58554b3
Guilherme <guilhermedejesusperdigao@gmail.com>
2026-04-30 14:18:45 +0000

    Adicionei loops aninhados (while e for) para simular a movimentação da peça Cavalo do Xadrez.

--- xadrez.c
+++ xadrez.c
@@ -35,6 +35,14 @@ int main() {
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
     printf("\n=== CAVALO ===\n");
+    while (cavalo <= 1) {
+        for (i = 1; i <= 2; i++) {
+            printf("Cima.\n");
+        }
+        printf("Direita.\n");
+        cavalo++;
+    }
+    
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
